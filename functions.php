886895a404d35ddbd92ee8182e1348291ac35b94
Ethan <e.thom.butler@gmail.com>
2017-07-18 14:29:54 -0400

    include image sizes in rest response

--- functions.php
+++ functions.php
@@ -23,7 +23,16 @@ add_action('init', function() use($child_dir, $parser) {
   // MUST run after loop in STB_Field_Group class
   foreach(get_post_types(['public' => true]) as $type){
     add_filter("rest_prepare_$type", function($res){
+      // Hide CMB2 labelling
       unset($res->data['cmb2']);
+
+      // Retrieve post thumbnail sizes
+      $id    = $res->data['featured_media'];
+      $sizes = get_intermediate_image_sizes();
+      foreach($sizes as $size){
+        $res->data['featured_media_objects'][] = wp_get_attachment_image_src($id, $size);
+      }
+
       return $res;
     }, 16, 1);
   }
